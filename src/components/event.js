c251093130f31f1b76014e0e2323171db93470d0
Toshiaki Furukawa <furukawa@serverworks.co.jp>
2020-06-27 12:46:30 +0900

    implement all events deletion feature

--- src/components/event.js
+++ src/components/event.js
@@ -3,10 +3,15 @@ import React from "react";
 const Event = ({ dispatch, event }) => {
   const id = event.id;
   const handleClickDeleteButton = () => {
-    dispatch({
-      type: "DELETE_EVENT",
-      id,
-    });
+    const result = window.confirm(
+      `${id}のイベントを本当に削除しても良いですか`
+    );
+    if (result) {
+      dispatch({
+        type: "DELETE_EVENT",
+        id,
+      });
+    }
   };
 
   return (
