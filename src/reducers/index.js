732a0875d3408e8eebbd45417de59b3eb8b0c47f
Toshiaki Furukawa <furukawa@serverworks.co.jp>
2020-06-27 15:48:12 +0900

    refactor events with reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,53 +1,5 @@
-import { CREATE_EVENT, DELETE_EVENT, DELETE_ALL_EVENTS } from "../actions";
-//action = {
-//   type: "CREATE_EVENT",
-//   title: "テスト",
-//   body: "ホゲホゲ"
-// }
-//
-//state = [
-//   {
-//     id: 1,
-//     title: "2020東京オリンピックのお知らせ",
-//     body: "2020年に東京でオリンピックを開催します、つきましては、、、、"
-//   }
-// ]
-//
-//# before
-//state = []
-//# after
-//state = [
-//  {
-//   id: 1,
-//   title: "テスト1",
-//   body: "ホゲホゲ"
-// },
-//  {
-//   id: 2,
-//   title: "テスト2",
-//   body: "ホゲホゲ"
-// },
-//{
-//   id: 3,
-//   title: "2020東京オリンピックのお知らせ",
-//   body: "2020年に東京でオリンピックを開催します、つきましては、、、、"
-// }]
-const events = (state = [], action) => {
-  switch (action.type) {
-    case CREATE_EVENT:
-      const event = { title: action.title, body: action.body };
-      const length = state.length;
-      const id = length === 0 ? 1 : state[length - 1].id + 1;
-      return [...state, { id, ...event }];
-    case DELETE_EVENT:
-      // stateの各々の要素のIDと、
-      //dispatchから渡ってきたaction.idが等しくないものを選択する
-      return state.filter((event) => event.id !== action.id);
-    case DELETE_ALL_EVENTS:
-      return [];
-    default:
-      return state;
-  }
-};
+import { combineReducers } from "redux";
 
-export default events;
+import events from "./events";
+
+export default combineReducers({ events });
