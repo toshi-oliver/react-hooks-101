6db0a6b4f14df00cd49bdd47ddc8998a2f6ceb92
Toshiaki Furukawa <furukawa@serverworks.co.jp>
2020-06-27 14:31:21 +0900

    repalce type names with constants

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import { CREATE_EVENT, DELETE_EVENT, DELETE_ALL_EVENTS } from "../actions";
 //action = {
 //   type: "CREATE_EVENT",
 //   title: "テスト",
@@ -33,16 +34,16 @@
 // }]
 const events = (state = [], action) => {
   switch (action.type) {
-    case "CREATE_EVENT":
+    case CREATE_EVENT:
       const event = { title: action.title, body: action.body };
       const length = state.length;
       const id = length === 0 ? 1 : state[length - 1].id + 1;
       return [...state, { id, ...event }];
-    case "DELETE_EVENT":
+    case DELETE_EVENT:
       // stateの各々の要素のIDと、
       //dispatchから渡ってきたaction.idが等しくないものを選択する
       return state.filter((event) => event.id !== action.id);
-    case "DELETE_ALL_EVENTS":
+    case DELETE_ALL_EVENTS:
       return [];
     default:
       return state;
