6fb0b65649895e4a9e6b3d82865ebe2a57e4e549
Toshiaki Furukawa <furukawa@serverworks.co.jp>
2020-06-27 16:03:58 +0900

    create operationLogs with reducers

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import reducer from "../reducers";
 const App = () => {
   const initialState = {
     events: [],
+    operationLogs: [],
   };
   const [state, dispatch] = useReducer(reducer, initialState);
 
